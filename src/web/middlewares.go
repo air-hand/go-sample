9ffadbaeba771e7409d85133ed803fd70365886c
air-hand <45233435+air-hand@users.noreply.github.com>
2021-09-16 23:19:10 +0900

    chore: add TODO comment.

--- src/web/middlewares.go
+++ src/web/middlewares.go
@@ -10,6 +10,7 @@ func DatabaseMiddleware(config *DBConnectConfig) func(http.Handler) http.Handler
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			db := NewDBClient(config)
+			// TODO: not avoid the cancelFunc
 			timeoutContext, _ := context.WithTimeout(context.Background(), time.Second)
 			ctx := context.WithValue(r.Context(), "DB", db.WithContext(timeoutContext))
 			next.ServeHTTP(w, r.WithContext(ctx))
