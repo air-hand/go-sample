3f976bac1fc716e83c1513e15b5799a992be9c5f
air-hand <45233435+air-hand@users.noreply.github.com>
2021-09-19 23:13:44 +0900

    wip: db-migrates

--- src/db-migrates/go.mod
+++ src/db-migrates/go.mod
@@ -5,3 +5,21 @@ go 1.17
 replace local.packages/web => ../web
 
 replace local.packages/types => ../types
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.14.1
+	local.packages/web v0.0.0-00010101000000-000000000000
+)
+
+require (
+	github.com/go-chi/chi/v5 v5.0.4 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/lestrrat-go/strftime v1.0.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	gorm.io/gorm v1.21.15 // indirect
+	local.packages/types v0.0.0 // indirect
+)
--- src/db-migrates/main.go
+++ src/db-migrates/main.go
@@ -4,21 +4,46 @@ package main
 
 import (
 	"embed"
-	// 2021/09/18 iofs has not already released yet
-	//	_ "github.com/golang-migrate/migrate/v4/source/iofs"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+
+	// TODO: iofs has not already released yet
+	// _ "github.com/golang-migrate/migrate/v4/source/iofs"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+
+	"local.packages/web"
 )
 
 //go:embed migrations/*.sql
 var fs embed.FS
 
 func main() {
-	//iofs.New(fs, "migrations")
+
+	var tmpdir string
+	tmpdir, err := os.MkdirTemp("", "migrations")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	// TODO: Write embed.FS to tempdir
+
 	// TODO: maybe should move to another module (neither web and HERE)
-	//	db := web.NewDBClient()
-	//	driver, err := mysql.WithInstance(db, &mysql.Config{})
-	//    m, err := migrate.NewWithDatabaseInstance(
-	//        "file:///migrations",
-	//        "mysql", driver)
-	//    )
-	//    m.Steps(2)
+	db_config := web.NewDBConnectConfigFromEnv()
+	db := web.NewDBClient(db_config)
+	defer db.Close()
+
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+	m, err := migrate.NewWithDatabaseInstance(
+		fmt.Sprintf("file://%s", tmpdir),
+		"mysql", driver,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	m.Steps(2)
 }
