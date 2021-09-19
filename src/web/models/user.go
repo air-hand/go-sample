3ab147fb6daad2e115904fa6e5d7b15ace23573a
air-hand <45233435+air-hand@users.noreply.github.com>
2021-09-19 23:14:17 +0900

    clean: remove gorm dependencies at all.

--- src/web/models/user.go
+++ src/web/models/user.go
@@ -1,14 +1,1 @@
 package models
-
-import "gorm.io/gorm"
-
-type User struct {
-	gorm.Model
-	Name   string
-	Groups []Group `gorm:"many2many:user_groups;"`
-}
-
-type Group struct {
-	gorm.Model
-	Name string
-}
